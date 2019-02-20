8a69d9db0b821567580140359c4de5cf36a565fd
David Cava <david.cava.pina@gmail.com>
2019-02-20 10:06:22 +0000

    aaa

---
+++ static/js/form.js
@@ -0,0 +1,172 @@
+Survey
+    .StylesManager
+    .applyTheme("default");
+
+var json = {
+    clearInvisibleValues: "onHidden",
+    questions: [
+        {
+            type: "radiogroup",
+            name: "age18",
+            title: "Are you 18 years old or older?",
+            isRequired: true,
+            choices: [
+                "Yes", "No"
+            ],
+            colCount: 0
+        }, {
+            type: "radiogroup",
+            name: "haveKids",
+            title: "Do you have a kid(s)?",
+            visibleIf: "{age18}='Yes'",
+            isRequired: true,
+            choices: [
+                "Yes", "No"
+            ],
+            colCount: 0
+        }, {
+            type: "dropdown",
+            name: "kids",
+            title: "How many kids do you have",
+            visibleIf: "{haveKids}='Yes'",
+            isRequired: true,
+            choices: [1, 2, 3, 4, 5]
+        }, {
+            type: "dropdown",
+            name: "kid1Age",
+            title: "The first kid age:",
+            visibleIf: "{kids} >= 1",
+            isRequired: true,
+            choices: [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16
+            ]
+        }, {
+            type: "dropdown",
+            name: "kid2Age",
+            title: "The second kid age:",
+            visibleIf: "{kids} >= 2",
+            isRequired: true,
+            startWithNewLine: false,
+            choices: [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16
+            ]
+        }, {
+            type: "dropdown",
+            name: "kid3Age",
+            title: "The third kid age:",
+            visibleIf: "{kids} >= 3",
+            isRequired: true,
+            startWithNewLine: false,
+            choices: [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16
+            ]
+        }, {
+            type: "dropdown",
+            name: "kid4Age",
+            title: "The fourth kid age:",
+            visibleIf: "{kids} >= 4",
+            isRequired: true,
+            startWithNewLine: false,
+            choices: [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16
+            ]
+        }, {
+            type: "dropdown",
+            name: "kid5Age",
+            title: "The fifth kid age:",
+            visibleIf: "{kids} >= 5",
+            isRequired: true,
+            startWithNewLine: false,
+            choices: [
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16
+            ]
+        }
+    ]
+};
+
+window.survey = new Survey.Model(json);
+
+survey
+    .onComplete
+    .add(function (result) {
+        document
+            .querySelector('#surveyResult')
+            .innerHTML = "result: " + JSON.stringify(result.data);
+    });
+
+$("#surveyElement").Survey({model: survey});
