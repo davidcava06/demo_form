4fb27c92a13b4779813489694cb9f0f678313da1
David Cava <david.cava.pina@gmail.com>
2019-02-20 11:35:05 +0000

    aa

--- static/js/form.js
+++ static/js/form.js
@@ -4,69 +4,58 @@
 
 var json = {
     clearInvisibleValues: "onHidden",
-    showProgressBar: "bottom",
-    goNextPageAutomatic: true,
-    pages:  [
+    questions: [
         {
-            questions: [
-                {
-                    type: "text",
-                    name: "name",
-                    title: "Name",
-                    placeHolder: "Enter Name",
-                    isRequired: true
-                },
-                {
-                    type: "text",
-                    name: "last_name",
-                    title: "Last Name",
-                    placeHolder: "Enter Last Name",
-                    isRequired: true
-                },
-                {
-                    type: "radiogroup",
-                    name: "gender",
-                    title: "Gender",
-                    isRequired: true,
-                    choices: [
-                        "M", "F", "Other"
-                    ],
-                    colCount: 0
-                }, 
-                {
-                    type: "radiogroup",
-                    name: "ethnicity",
-                    title: "Ethnicity Group",
-                    isRequired: true,
-                    choices: [
-                        "National", "Non-National"
-                    ],
-                    colCount: 0
-                }, 
-                {
-                    type: "text",
-                    name: "age_group",
-                    title: "Age Group",
-                    placeHolder: "Enter age",
-                    isRequired: true
-                }
-            ]
+            type: "text",
+            name: "name",
+            title: "Name",
+            placeHolder: "Enter Name",
+            isRequired: true
         },
         {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "haveKids",
-                    title: "Do you have a kid(s)?",
-                    visibleIf: "{age}>=17",
-                    isRequired: true,
-                    choices: ["Yes", "No"],
-                    colCount: 0
-                }
-            ]
+            type: "text",
+            name: "last_name",
+            title: "Last Name",
+            placeHolder: "Enter Last Name",
+            isRequired: true
+        },
+        {
+            type: "radiogroup",
+            name: "gender",
+            title: "Gender",
+            isRequired: true,
+            choices: [
+                "M", "F", "Other"
+            ],
+            colCount: 0
+        }, 
+        {
+            type: "radiogroup",
+            name: "ethnicity",
+            title: "Ethnicity Group",
+            isRequired: true,
+            choices: [
+                "National", "Non-National"
+            ],
+            colCount: 0
+        }, 
+        {
+            type: "text",
+            name: "age_group",
+            title: "Age Group",
+            placeHolder: "Enter age",
+            isRequired: true
+        },
+        {
+            type: "radiogroup",
+            name: "haveKids",
+            title: "Do you have a kid(s)?",
+            visibleIf: "{age}>=17",
+            isRequired: true,
+            choices: ["Yes", "No"],
+            colCount: 0
         }
-    ]
-    
+    ]    
 };
 
 window.survey = new Survey.Model(json);
